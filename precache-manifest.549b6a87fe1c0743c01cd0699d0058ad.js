6fdd92768513f7262a35a0b3331c50b2d29951e4
vanssign <thindvansh1999@gmail.com>
2020-09-21 13:18:30 +0530

    Updates

---
+++ precache-manifest.549b6a87fe1c0743c01cd0699d0058ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76bc8ec094ae8d9dca6c5822a90678d",
+    "url": "/resume-spa/index.html"
+  },
+  {
+    "revision": "ed20fefc1d5004fab012",
+    "url": "/resume-spa/static/css/2.090e4a27.chunk.css"
+  },
+  {
+    "revision": "8bd8a2243f6131e86224",
+    "url": "/resume-spa/static/css/main.0de3f03e.chunk.css"
+  },
+  {
+    "revision": "ed20fefc1d5004fab012",
+    "url": "/resume-spa/static/js/2.3a1a1f04.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/resume-spa/static/js/2.3a1a1f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd8a2243f6131e86224",
+    "url": "/resume-spa/static/js/main.821f9e82.chunk.js"
+  },
+  {
+    "revision": "4ba159e81de65e3b928e",
+    "url": "/resume-spa/static/js/runtime-main.1de182e7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/resume-spa/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/resume-spa/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/resume-spa/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/resume-spa/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/resume-spa/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
